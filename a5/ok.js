d191f6b8a84d2f4280d07b2ee195e6b4bf71d663
John <John@qq.com>
2024-07-21 10:34:02 +0800

    update ads

--- a5/ok.js
+++ a5/ok.js
@@ -9,8 +9,10 @@ function init() {
     "https://classic-bonus.com/b.3LVa0ePZ3rp/v/bqmWVCJOZcDW0b1_MIzgcByyMkzmAVy_L/TvUzzVNBzoIoz/MyDAML",
     "https://classic-bonus.com/b.3VVj0NPQ3/pEv/bLmwVXJ/ZjDB0I1lM/zvcAyoM/zHEUw/LdTRUOz_NazAI/zyMRTZEv",
     
-    // hil: Michael(oktu)
-    "https://spry-cat.com/bJ3DV.0UPE3Ep/vYbUmzVWJLZhDb0/1HNaDfUPytM/DWUuy-LETdUP0HN_TiIQwMNbTxMU",
+    // // hil: Michael(oktu)
+    // "https://spry-cat.com/bJ3DV.0UPE3Ep/vYbUmzVWJLZhDb0/1HNaDfUPytM/DWUuy-LETdUP0HN_TiIQwMNbTxMU",
+    // hil: Michael(yoo)
+    "https://spry-cat.com/by3zVN0AP.3qpKvwbvmEVBJaZODG0M1UNHDGQE5xOHDYY/2DLnToUF0/N/D/kz4/NPjLca",
 
     // monetag: db
     "https://teksishe.net/4/7420698",
