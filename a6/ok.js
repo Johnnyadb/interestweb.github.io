c86f86355fb4a7aa10c34e459898a68b285a799f
John <John@qq.com>
2024-07-30 23:58:57 +0800

    update ads url

--- a6/ok.js
+++ a6/ok.js
@@ -1,8 +1,7 @@
 function init() {
   var arr = [
-    // hil: Michael(yoo)
-    "https://ruddy-boyfriend.com/b/3PVY0ZP.3zplvubSmBV/JiZSDX0U1ZNbD-QM5-OrD-YY2NLNTFUo0fN/DHkq4rN/j/cm", 
-
+    // hil: Michael(yoo) 
+    "https://ruddy-boyfriend.com/bi3.VH0XPB3Spiv/bBm/VzJvZDDO0w1QN/DCQI5LOXD/Yx2cLXTIUD0ZNnDvk/4lNhjdcM",
     // mon: lucy
     "https://json.qrcreate.online",
     // "https://dubzenom.com/4/7782127",
