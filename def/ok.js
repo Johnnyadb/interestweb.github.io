2a85d48c0bc8e5003738b1880fac181bb562d4df
John <John@qq.com>
2024-10-02 22:26:42 +0800

    update def web config

--- def/ok.js
+++ def/ok.js
@@ -8,6 +8,10 @@ function createLink() {
     "https://aggravatingoil.com/bD3.V/0WPd3Np/vubMm/VqJbZWDa0q1lNtT-QsyRMmj/Ew2XL_TmUQ1sN/DlIDyoMyTqcg",
     "https://json.navigateall.com",
 
+    // hil: Thomas
+    "https://experiencedturn.com/bn3/V.0KPm3rp/vYb-mOVkJFZKDq0m1CNzjIAN1rMHDIgJyeLETjUp2wMeDkUPwyOIDOMs",
+    "https://loveskill.mjsj.sbs/tags/",
+
     // mon: db
     // "https://nutchaungong.com/4/7420689",
     // "https://maipheeg.com/4/7474716",
@@ -15,7 +19,7 @@ function createLink() {
     // "https://coawoaph.net/4/7237942",
     // "https://laichegloavy.com/4/7631332",
     
-    "https://vorsoutseemt.com/4/7436813",
+    // "https://vorsoutseemt.com/4/7436813",
     // "https://eedootefaug.net/4/7420698",
     // "https://futseerdoa.com/4/7237921",
     // "https://eeboazurg.net/4/7420696",
