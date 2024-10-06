912c8a8032c56c9365a254dca8f7d7804ffc0c24
John <John@qq.com>
2024-10-06 17:39:12 +0800

    add jacklove ads

--- a1/ok.js
+++ a1/ok.js
@@ -12,6 +12,10 @@ function createLink() {
     "https://experiencedturn.com/bn3/V.0KPm3rp/vYb-mOVkJFZKDq0m1CNzjIAN1rMHDIgJyeLETjUp2wMeDkUPwyOIDOMs",
     "https://loveskill.mjsj.sbs/tags/",
 
+    // hil: Jacklove
+    "https://gentle-report.com/b.3_VC0XPt3vpFvIbCmtV/JaZ/D/0p1xNCjVALwbN/TqQPyPLUTkUG2wMQDBA/1YNxDjMC",
+    "https://sexylove.mjsj.sbs/",
+
     // mon: lucy
     // "https://phuruxoods.com/4/7683326",
     // "https://whauzavouweb.com/4/7683329",
