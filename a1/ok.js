53f4ce0db707e1cf3443ae778b4cec7585f914fe
John <John@qq.com>
2024-10-16 01:59:39 +0800

    update ads

--- a1/ok.js
+++ a1/ok.js
@@ -14,6 +14,7 @@ function createLink() {
     // hil: Thomas
     "https://experiencedturn.com/bn3/V.0KPm3rp/vYb-mOVkJFZKDq0m1CNzjIAN1rMHDIgJyeLETjUp2wMeDkUPwyOIDOMs",
     // "https://loveskill.mjsj.sbs/tags/",
+    "https://sexytalk.mjsj.sbs/tags/",
 
     // hil: Jacklove
     "https://sexylove.mjsj.sbs/tags/",
@@ -25,11 +26,11 @@ function createLink() {
     // "https://coawoaph.net/4/7237942",
     // "https://laichegloavy.com/4/7631332",
     
-    "https://vorsoutseemt.com/4/7436813",
-    "https://eedootefaug.net/4/7420698",
-    "https://futseerdoa.com/4/7237921",
-    // "https://eeboazurg.net/4/7420696",
-    // "https://psauwaun.com/4/7614690",
+    // "https://vorsoutseemt.com/4/7436813",
+    // "https://eedootefaug.net/4/7420698",
+    // "https://futseerdoa.com/4/7237921",
+    "https://eeboazurg.net/4/7420696",
+    "https://psauwaun.com/4/7614690",
     // "https://ptougeegnep.net/4/7237907",
 
     // mon: lucy
