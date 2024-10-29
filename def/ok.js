bed720cab7faf4e8dc022e1e9fcb2febfff1a2cb
John <John@qq.com>
2024-10-29 21:14:20 +0800

    update web ad config

--- def/ok.js
+++ def/ok.js
@@ -3,13 +3,13 @@ function createLink() {
     // hil: db (ss)
     "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
     "https://www.qrcreate.online",
-
-    // hil: db123
-    "https://sexystory.navigateall.com/tags/",
     
     // hil: Brian
     "https://aggravatingoil.com/bD3.V/0WPd3Np/vubMm/VqJbZWDa0q1lNtT-QsyRMmj/Ew2XL_TmUQ1sN/DlIDyoMyTqcg",
     "https://json.navigateall.com",
+
+    // hil: db123
+    "https://sexystory.navigateall.com/tags/",
 
     // // hil: Thomas
     // "https://experiencedturn.com/bn3/V.0KPm3rp/vYb-mOVkJFZKDq0m1CNzjIAN1rMHDIgJyeLETjUp2wMeDkUPwyOIDOMs",
@@ -20,18 +20,18 @@ function createLink() {
     "https://sexylove.mjsj.sbs/tags/",
 
     // mon: db
-    "https://nutchaungong.com/4/7420689",
-    "https://maipheeg.com/4/7474716",
+    // "https://nutchaungong.com/4/7420689",
+    // "https://maipheeg.com/4/7474716",
     // "https://glocheexi.com/4/7237941",
     // "https://coawoaph.net/4/7237942",
     // "https://laichegloavy.com/4/7631332",
     
     // "https://vorsoutseemt.com/4/7436813",
     // "https://eedootefaug.net/4/7420698",
-    // "https://futseerdoa.com/4/7237921",
-    // "https://eeboazurg.net/4/7420696",
-    // "https://psauwaun.com/4/7614690",
-    // "https://ptougeegnep.net/4/7237907",
+    "https://futseerdoa.com/4/7237921",
+    "https://eeboazurg.net/4/7420696",
+    "https://psauwaun.com/4/7614690",
+    "https://ptougeegnep.net/4/7237907",
   ];
 
   if (urls.length === 0) {
