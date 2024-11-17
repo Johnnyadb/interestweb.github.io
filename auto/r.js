0f9530fdd6d33b76181ac4ceb269c1da2d7114a1
John <John@qq.com>
2024-11-17 20:01:04 +0800

    增加权重控制

--- auto/r.js
+++ auto/r.js
@@ -1,12 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
   const links = [
-      "https://baidu.com",
-      "https://google.com",
-      "https://360.com",
+      // hil: Jacklove: sexylove
+      {
+        // jm
+        url: "https://diligentcloset.com/b/3.Vv0/PI3mpLv-b/mpV/JCZfD/0R1oNwjjUP1/MSjWcg2HLoTSUZ2JN/TCUWylNaztcm",
+        w: 3
+      },
+      {
+        // sl
+        url: "https://diligentcloset.com/b/3.Vj0tPv3xpNvebPmtVcJ/Z/DH0n1_N/j-EV1DMnTrkF4SLMT/UO2KMrTNUDx/OcTIkR",
+        w: 2
+      },
+      // hil: Jacklove: je
+      {
+        url: "https://diligentcloset.com/b.3QVo0/Pd3OpgvMbcmyVcJWZADx0m1VNejQcw3sOdTrER0sLHTmUt2kN_z/cN5hMIT_UK",
+        w: 1
+      }
   ];
 
-  const index = new Date().getSeconds() % links.length;
-  const redirectUrl = links[index];
-
+  function selectLinkByWeight(links) {
+    const totalWeight = links.reduce((acc, link) => acc + link.w, 0);
+    let random = Math.random() * totalWeight;
+    for (const link of links) {
+        random -= link.w;
+        if (random < 0) {
+            return link.url;
+        }
+    }
+  }
+  const redirectUrl = selectLinkByWeight(links);
   window.location.href = redirectUrl;
 });
