3611bbc6b6a8095592d2ce501d06f50798767ea9
John <John@qq.com>
2024-12-09 19:16:34 +0800

    提升def2广告展示频率

--- def/ok.js
+++ def/ok.js
@@ -1,12 +1,12 @@
 function createLink() {
   var urls = [
     // hil: db (ss)
-    "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
     "https://www.qrcreate.online",
+    // "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
     
     // hil: Brian
-    "https://aggravatingoil.com/bD3.V/0WPd3Np/vubMm/VqJbZWDa0q1lNtT-QsyRMmj/Ew2XL_TmUQ1sN/DlIDyoMyTqcg",
     "https://json.navigateall.com",
+    // "https://aggravatingoil.com/bD3.V/0WPd3Np/vubMm/VqJbZWDa0q1lNtT-QsyRMmj/Ew2XL_TmUQ1sN/DlIDyoMyTqcg",
 
     // hil: db123
     "https://sexystory.navigateall.com/tags/",
@@ -23,12 +23,12 @@ function createLink() {
     // "https://maipheeg.com/4/7474716",
     // "https://glocheexi.com/4/7237941",
     // "https://coawoaph.net/4/7237942",
-    "https://laichegloavy.com/4/7631332",
+    // "https://laichegloavy.com/4/7631332",
     
-    "https://vorsoutseemt.com/4/7436813",
-    "https://eedootefaug.net/4/7420698",
-    // "https://futseerdoa.com/4/7237921",
-    // "https://eeboazurg.net/4/7420696",
+    // "https://vorsoutseemt.com/4/7436813",
+    // "https://eedootefaug.net/4/7420698",
+    "https://futseerdoa.com/4/7237921",
+    "https://eeboazurg.net/4/7420696",
     // "https://psauwaun.com/4/7614690",
     // "https://ptougeegnep.net/4/7237907",
   ];
