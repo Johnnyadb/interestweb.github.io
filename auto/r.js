c2a36ce233edac728723156c86fc33e1ada0e332
John <John@qq.com>
2024-12-09 16:56:22 +0800

    mon ad展示广告频率调整

--- auto/r.js
+++ auto/r.js
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function() {
         urls: [
           "https://luglawhaulsano.net/4/8628644",
           "https://luglawhaulsano.net/4/8628640",
-          // "https://luglawhaulsano.net/4/8628645",
+          "https://luglawhaulsano.net/4/8628645",
           // "https://luglawhaulsano.net/4/8628429",
           // "https://luglawhaulsano.net/4/8628428",
         ],
