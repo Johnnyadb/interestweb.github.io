447cc4ca6e40f12ae62a41d9adb33ef7f7dfc626
John <John@qq.com>
2024-12-09 19:54:57 +0800

    update mon ads

--- auto/r.js
+++ auto/r.js
@@ -25,13 +25,13 @@ document.addEventListener('DOMContentLoaded', function() {
       // hil: Thomas/Mary
       {
         url: "https://superficial-sensitive.com/bX3IV.0UPE3_pxvFblmtVvJRZVDT0/1bOcD/Mr0WNzjDcAwtLIT/Uw4aMSz/QR2_NQzwEo",
-        w: 0.5
+        w: 0.7
       },
 
       // hil: yangjin/James
       {
         url: "https://jubilantsignal.com/ba3AV.0iP-3Cp_vZbfmhVbJZZLDH0-1OOFDzMr5OO/DzEs5OLNTbUq4uM/z/k/4/M/jRAP",
-        w: 0.5
+        w: 0.6
       },
 
       // hil: Brian/Mary 
@@ -51,10 +51,10 @@ document.addEventListener('DOMContentLoaded', function() {
           "https://luglawhaulsano.net/4/8628644",
           "https://luglawhaulsano.net/4/8628640",
           "https://luglawhaulsano.net/4/8628645",
-          // "https://luglawhaulsano.net/4/8628429",
-          // "https://luglawhaulsano.net/4/8628428",
+          "https://luglawhaulsano.net/4/8628429",
+          "https://luglawhaulsano.net/4/8628428",
         ],
-        w: 0.6
+        w: 1
       },
   ];
 
