8d25f628fc9b4ce41be1804367f7b8f14cbbde45
John <John@qq.com>
2024-12-11 19:59:54 +0800

    update web ad config

--- def/ok.js
+++ def/ok.js
@@ -2,11 +2,11 @@ function createLink() {
   var urls = [
     // hil: db (ss)
     "https://www.qrcreate.online",
-    // "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
+    "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
     
     // hil: Brian
     "https://json.navigateall.com",
-    // "https://aggravatingoil.com/bD3.V/0WPd3Np/vubMm/VqJbZWDa0q1lNtT-QsyRMmj/Ew2XL_TmUQ1sN/DlIDyoMyTqcg",
+    "https://aggravatingoil.com/bD3.V/0WPd3Np/vubMm/VqJbZWDa0q1lNtT-QsyRMmj/Ew2XL_TmUQ1sN/DlIDyoMyTqcg",
 
     // hil: db123
     "https://sexystory.navigateall.com/tags/",
@@ -27,9 +27,9 @@ function createLink() {
     
     // "https://vorsoutseemt.com/4/7436813",
     // "https://eedootefaug.net/4/7420698",
-    "https://futseerdoa.com/4/7237921",
+    // "https://futseerdoa.com/4/7237921",
     "https://eeboazurg.net/4/7420696",
-    // "https://psauwaun.com/4/7614690",
+    "https://psauwaun.com/4/7614690",
     // "https://ptougeegnep.net/4/7237907",
   ];
 
