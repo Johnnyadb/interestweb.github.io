3b577e203baff675f9d01569078bcd4fa26da766
John <John@qq.com>
2024-12-13 11:49:03 +0800

    更新ad展示频率

--- auto/r.js
+++ auto/r.js
@@ -36,17 +36,17 @@ document.addEventListener('DOMContentLoaded', function() {
       // hil: Thomas/Mary
       {
         url: "https://superficial-sensitive.com/bX3IV.0UPE3_pxvFblmtVvJRZVDT0/1bOcD/Mr0WNzjDcAwtLIT/Uw4aMSz/QR2_NQzwEo",
-        w: 0.7
+        w: 0.8
       },
       // hil: yangjin/James
       {
         url: "https://jubilantsignal.com/ba3AV.0iP-3Cp_vZbfmhVbJZZLDH0-1OOFDzMr5OO/DzEs5OLNTbUq4uM/z/k/4/M/jRAP",
-        w: 0.6
+        w: 0.7
       },
       // hil: goodboy/Denis
       {
         url: "https://happygolucky-officer.com/bS3.VL0zPs3dpwvCb/mfVYJkZAD/0X1wOSDhcWz/MEDjUGxYLdT_U/4PNgzrMOwkN/TkIk",
-        w: 0.2,
+        w: 0.4,
       },
 
       // mon: Brian
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
           "https://luglawhaulsano.net/4/8628429",
           // "https://luglawhaulsano.net/4/8628428",
         ],
-        w: 0.6
+        w: 0.8
       },
       {
         urls: [
