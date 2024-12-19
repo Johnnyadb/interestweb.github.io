79ceac146521700263751406f6caeea945e27b40
John <John@qq.com>
2024-12-20 02:28:16 +0800

    add mon-Brian auto redirect page

---
+++ a14/auto-mon-brian/r.js
@@ -0,0 +1,23 @@
+onPageVisibilityChange(async function() {
+  const links = [
+    // mon: Brian
+    {
+      urls: [
+        // "https://luglawhaulsano.net/4/8628644",
+        // "https://luglawhaulsano.net/4/8628640",
+        "https://luglawhaulsano.net/4/8628645",
+        "https://luglawhaulsano.net/4/8628429",
+        "https://luglawhaulsano.net/4/8628428",
+      ],
+      w: 2.4
+    },
+    {
+      urls: [
+        // hil: Brian
+        "https://aggravatingoil.com/bD3.V/0WPd3Np/vubMm/VqJbZWDa0q1lNtT-QsyRMmj/Ew2XL_TmUQ1sN/DlIDyoMyTqcg",
+      ],
+      w: 10.0
+    }    
+  ];
+  redirectSelectLinkByWeight(links);
+});
