cda05fd2771b8595be45198d326c6edbeb734032
John <John@qq.com>
2024-12-22 11:12:38 +0800

    update web config

--- a14/auto-mon-brian/r.js
+++ a14/auto-mon-brian/r.js
@@ -5,11 +5,11 @@ onPageVisibilityChange(async function() {
       urls: [
         "https://luglawhaulsano.net/4/8628644",
         "https://luglawhaulsano.net/4/8628640",
-        // "https://luglawhaulsano.net/4/8628645",
+        "https://luglawhaulsano.net/4/8628645",
         // "https://luglawhaulsano.net/4/8628429",
-        "https://luglawhaulsano.net/4/8628428",
+        // "https://luglawhaulsano.net/4/8628428",
       ],
-      w: 3.0
+      w: 3.5
     },
     {
       urls: [
--- auto/r.js
+++ auto/r.js
@@ -25,18 +25,18 @@ onPageVisibilityChange(async function() {
       // hil: Thomas/Mary
       {
         url: "https://superficial-sensitive.com/bX3IV.0UPE3_pxvFblmtVvJRZVDT0/1bOcD/Mr0WNzjDcAwtLIT/Uw4aMSz/QR2_NQzwEo",
-        w: 1.2
+        w: 1.3
       },
       // hil: yangjin/James
       {
         // high
         url: "https://jubilantsignal.com/bC3/Vy0oP.3_p/vkbmmuVYJlZ-DA0C1cOVT/MO2DOwT/IQ5eLPTrUX5jMfzHYm5/Mqz/Al",
-        w: 1.0
+        w: 1.1
       },
       // hil: goodboy/Denis
       {
         url: "https://happygolucky-officer.com/bS3.VL0zPs3dpwvCb/mfVYJkZAD/0X1wOSDhcWz/MEDjUGxYLdT_U/4PNgzrMOwkN/TkIk",
-        w: 0.8,
+        w: 0.9,
       },
   ];
   redirectSelectLinkByWeight(links);
