8587157dc8b3c234f8095955d3412ab9caf8a757
John <John@qq.com>
2024-12-24 08:13:30 +0800

    update auto config

--- auto/r.js
+++ auto/r.js
@@ -4,12 +4,12 @@ onPageVisibilityChange(async function() {
       {
         // jm
         url: "https://diligentcloset.com/b/3.Vv0/PI3mpLv-b/mpV/JCZfD/0R1oNwjjUP1/MSjWcg2HLoTSUZ2JN/TCUWylNaztcm",
-        w: 4
+        w: 3
       },
       {
         // sl
         url: "https://diligentcloset.com/b/3.Vj0tPv3xpNvebPmtVcJ/Z/DH0n1_N/j-EV1DMnTrkF4SLMT/UO2KMrTNUDx/OcTIkR",
-        w: 5
+        w: 3
       },
 
       // hil: db123: sexystory
@@ -19,24 +19,24 @@ onPageVisibilityChange(async function() {
           "https://reasonable-source.com/bS3BVo0xP.3dpivWbwmUVzJxZRDg0D1BNlT/cpw/NGzvEv4LLBT_UD1ANSzjA/3YMoTlkP",
           "https://reasonable-source.com/ba3bV/0.PX3/pevYbzmAVAJoZvDf0e1_N/TocSwPNJTlcfyhLFTYUH1HNuzTAA1/NdzPMy",
         ],
-        w: 4
+        w: 3
       },
 
       // hil: Thomas/Mary
       {
         url: "https://superficial-sensitive.com/bX3IV.0UPE3_pxvFblmtVvJRZVDT0/1bOcD/Mr0WNzjDcAwtLIT/Uw4aMSz/QR2_NQzwEo",
-        w: 1.3
+        w: 6
       },
       // hil: yangjin/James
       {
         // high
         url: "https://jubilantsignal.com/bC3/Vy0oP.3_p/vkbmmuVYJlZ-DA0C1cOVT/MO2DOwT/IQ5eLPTrUX5jMfzHYm5/Mqz/Al",
-        w: 1.1
+        w: 6
       },
       // hil: goodboy/Denis
       {
         url: "https://happygolucky-officer.com/bS3.VL0zPs3dpwvCb/mfVYJkZAD/0X1wOSDhcWz/MEDjUGxYLdT_U/4PNgzrMOwkN/TkIk",
-        w: 0.9,
+        w: 6,
       },
   ];
   redirectSelectLinkByWeight(links);
