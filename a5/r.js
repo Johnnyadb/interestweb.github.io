da42d95d04fd687146dd131d54e4018da24d1226
John <John@qq.com>
2024-12-29 10:54:37 +0800

    update web config

--- a5/r.js
+++ a5/r.js
@@ -8,11 +8,11 @@ function createLink() {
     "https://interestweb.mjsj.sbs/def/auto-mon-brian",
 
     // mon: db
-    "https://nutchaungong.com/4/7420689",
-    "https://maipheeg.com/4/7474716",
+    // "https://nutchaungong.com/4/7420689",
+    // "https://maipheeg.com/4/7474716",
     "https://glocheexi.com/4/7237941",
-    // "https://coawoaph.net/4/7237942",
-    // "https://laichegloavy.com/4/7631332",
+    "https://coawoaph.net/4/7237942",
+    "https://laichegloavy.com/4/7631332",
     
     // "https://vorsoutseemt.com/4/7436813",
     // "https://eedootefaug.net/4/7420698",
