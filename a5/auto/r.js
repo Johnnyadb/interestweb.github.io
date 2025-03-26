4479c8b6481f42de7450d80b529f4690300838d0
John <John@qq.com>
2025-03-26 23:29:19 +0800

    update web config

--- a5/auto/r.js
+++ a5/auto/r.js
@@ -11,14 +11,14 @@ onPageVisibilityChange(async function() {
           // hil: ThomasFriends123/Mary
           // chong
           "https://superficial-sensitive.com/bX3IV.0UPE3_pxvFblmtVvJRZVDT0/1bOcD/Mr0WNzjDcAwtLIT/Uw4aMSz/QR2_NQzwEo",
-          // pretty
-          "https://mortifiedmom.com/b.3LVa0/P/3ppVv/bAmpVNJ-ZgDI0E2_MoTlMgxWMsT/Iey/LtT/YXxWMYzFEfxeMej/Mm",
+          // // pretty
+          // "https://mortifiedmom.com/b.3LVa0/P/3ppVv/bAmpVNJ-ZgDI0E2_MoTlMgxWMsT/Iey/LtT/YXxWMYzFEfxeMej/Mm",
 
           // hil: protonsgoodboy/Denis
           // kang
           "https://happygolucky-officer.com/bS3.VL0zPs3dpwvCb/mfVYJkZAD/0X1wOSDhcWz/MEDjUGxYLdT_U/4PNgzrMOwkN/TkIk",
-          // sexygirl
-          "https://salty-counter.com/bj3MVZ0NP.3_pvv/b/m/VgJyZtDt0d2OMkTMID5dNZTfA/4fLmTdYyxFM/jqkq1AM/DDkI",
+          // // sexygirl
+          // "https://salty-counter.com/bj3MVZ0NP.3_pvv/b/m/VgJyZtDt0d2OMkTMID5dNZTfA/4fLmTdYyxFM/jqkq1AM/DDkI",
       
           // clickadilla: Lucy
           "https://hot123.top/tags/",
