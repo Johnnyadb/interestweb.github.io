135319d474b54d3bbe784225c6a6c40c4c11ed1f
John <John@qq.com>
2025-04-12 20:28:53 +0800

    update mon web config

--- a5/r.js
+++ a5/r.js
@@ -7,11 +7,11 @@ function createLink() {
     "https://tidy-status.com/BskeKo",
     
     // mon: db
-    "https://laichegloavy.com/4/7631332",
+    // "https://laichegloavy.com/4/7631332",
     "https://vorsoutseemt.com/4/7436813",
     "https://eedootefaug.net/4/7420698",
     "https://futseerdoa.com/4/7237921",
-    // "https://eeboazurg.net/4/7420696",
+    "https://eeboazurg.net/4/7420696",
     // "https://psauwaun.com/4/7614690",
     // "https://ptougeegnep.net/4/7237907",
   ];
