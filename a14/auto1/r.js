e29b80359452c2a6c316f3ff3019c6bc28ea1f3d
John <John@qq.com>
2025-05-17 11:48:27 +0800

    添加a14/auto1，和a14/auto相同，逐步废弃a14/auto

---
+++ a14/auto1/r.js
@@ -0,0 +1,35 @@
+onPageVisibilityChange(async function() {
+  const links = [
+    {
+      urls: [
+        // hil: db (ss)
+        "https://grim-package.com/b.3yVi0OPz3/pDvqblmoVxJWZ_DC0/1ZNhDOM/2KO/TacQ2HL/TiUO0JMmzLYw5-NizhcK",
+        "https://www.qrcreate.online",
+        "https://tidy-status.com/BskeKo",
+
+        // hil: ThomasFriends123/Mary
+        // chong
+        // "https://superficial-sensitive.com/bX3IV.0UPE3_pxvFblmtVvJRZVDT0/1bOcD/Mr0WNzjDcAwtLIT/Uw4aMSz/QR2_NQzwEo",
+
+        // hil: protonsgoodboy/Denis
+        // kang
+        // "https://happygolucky-officer.com/bS3.VL0zPs3dpwvCb/mfVYJkZAD/0X1wOSDhcWz/MEDjUGxYLdT_U/4PNgzrMOwkN/TkIk",
+    
+        // clickadilla: Lucy
+        "https://hot123.top/tags/",
+        // clickadilla: db123
+        "https://sexylove.mjsj.sbs",
+        // clickadilla: Brian
+        "https://json.qrcreate.online",
+
+        // mon: db
+        "https://ougeechimp.net/4/9249984",
+        "https://bulrovufaiphi.com/4/9242232",
+        "https://bulrovufaiphi.com/4/9242113",
+        "https://bulrovufaiphi.com/4/7420698",
+      ],
+      w: 1.0
+    }  
+  ];
+  redirectSelectLinkByWeight(links);
+});
