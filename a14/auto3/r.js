754445f25d011b488fde4fe7799c914dbb2bd5ea
John <John@qq.com>
2025-06-16 20:00:13 +0800

    提高a14/auto3的mon和hil广告频率

--- a14/auto3/r.js
+++ a14/auto3/r.js
@@ -18,7 +18,7 @@ onPageVisibilityChange(async function() {
         // clickadilla: Brian
         "https://json.qrcreate.online",
       ],
-      w: 6.0
+      w: 4.0
     },
     // mon: db
     {
